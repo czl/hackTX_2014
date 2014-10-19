6b1fc562955b5bcdd7c0d5a192d7ee863e840f9d
czl <clee508@gmail.com>
2014-10-18 22:36:21 -0500

    game 1 working
    preliminary game working, now just have to tweak on the timing

--- game_one/game_one.ino
+++ game_one/game_one.ino
@@ -26,7 +26,14 @@ unsigned int ptr0 = 0;
 unsigned int ptr1 = 0;
 unsigned int ptr_max = 16; //when pointer gets to index 15 we know that is the last part of map
 
+int val0 = 0;
+int val1 = 0;
+int user0 = 0;
+int user1 = 0;
+
+boolean correct_press();//checks to see if the user pressed the correct buttons
 void map_display();//const unsigned int& map0, const unsigned int& map1, unsigned int& p0, unsigned int& p1);
+
 
 void setup() 
 {    
@@ -49,6 +56,7 @@ void setup()
     lcd.print(blank_writer);
     delay(200);
     map_display();
+    delay(500);
 }
 
 //map display function
@@ -56,6 +64,24 @@ void map_display()//const unsigned int& map0, const unsigned int& map1, unsigned
 {
   lcd.setCursor(0,0);
   int i = 0;
+  //setting val0 and val1
+  val0 = map_0[ptr0];
+  val1 = map_1[ptr1];
+  /*lcd.setCursor(0,0);
+  lcd.print("val0:");
+  lcd.print(val0);
+  lcd.setCursor(0,1);
+  lcd.print("val1:");
+  lcd.print(val1);
+  delay(700);
+  lcd.setCursor(0,0);
+  lcd.print("usr0:");
+  lcd.print(user0);
+  lcd.setCursor(0,1);
+  lcd.print("usr1:");
+  lcd.print(user1);
+  delay(700);*/
+  //displaying map
   for(;i<16;++i)
   {
     lcd.setCursor(i,0);
@@ -69,17 +95,75 @@ void map_display()//const unsigned int& map0, const unsigned int& map1, unsigned
     else
       lcd.print(" ");
   } 
+  //delay(700);
+}
+
+boolean correct_press()
+{
+  /*lcd.setCursor(0,0);
+  lcd.print(user0);
+  lcd.setCursor(0,1);
+  lcd.print(user1);
+  delay(700);
+   if(user0==val0 && user1==val1)
+   {
+     tone(buzz_pin, 1500, 400);
+     lcd.setCursor(0,0);
+     lcd.print("CORRECT");
+     delay(900);
+   }
+   else{
+     //tone(buzz_pin, 2000, 300);
+     //tone(buzz_pin, 400, 400);
+     lcd.setCursor(0,0);
+     lcd.print("~~~~~~");
+     lcd.setCursor(0,1);
+     lcd.print("~~~~~~"); 
+     delay(900);
+   }*/
+   //delay(100);
+   return (user0==val0 && user1==val1);
 }
 
 void loop() 
 {
-  while(ptr0<ptr_max)
-  { 
-   delay(700);
+  button_state = digitalRead(button_pin);
+  touch_state = digitalRead(touch_pin);
+//  lcd.setCursor(0,0);
+//  lcd.print(touch_state);
+//  lcd.setCursor(0,1);
+//  lcd.print(button_state);
+//  delay(100);
+//  tone(buzz_pin, 900, 40);
+  if(button_state == HIGH)
+  {
+    user1 = 1;
+    /*tone(buzz_pin, 900, 200);
+    lcd.setCursor(0,1);
+    lcd.print("HIHIHIHI");*/
+  }
+  else
+    user1 = 0;
+  if(touch_state == HIGH)
+  {
+    user0 = 1;
+    /*tone(buzz_pin, 1800, 40);
+    lcd.setCursor(0,0);
+    lcd.print("00000");*/
+  }
+  else
+    user0 = 0;
+  if(correct_press())
+  {
     ++ptr0;
-    ++ptr1;
+    ++ptr1; 
+  }
+  if(ptr0<ptr_max)
+  { 
+   //delay(100);
     map_display();
   }
+  
   if(ptr0>=ptr_max)
   {
     lcd.setCursor(0,0);
@@ -87,8 +171,11 @@ void loop()
     lcd.print(blank_writer);
     lcd.setCursor(0,0);
     lcd.print("DONE! GAME OVER");
+    lcd.setCursor(0,1);
+    lcd.print(blank_writer);
     while(true){}
   }
+  delay(10);
 }
 
 /*********************************************************************************************************
